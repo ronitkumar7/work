d2ad91f64538284d02770345bc0769873906565c
Wilson Sy <sy.wilson@protonmail.com>
2020-11-29 22:18:29 -0500

    Add driver structure for main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import data_formatting
+import vader_analysis
+import stats_analysis
+
+if __name__ == "__main__":
+
+    # Data formatting
+    f = open('dataset.csv', 'r')
+
+    dataset = data_formatting.process(f)
+
+    # VADER sentiment analysis
+    vader_analysis.analyze(dataset)
+
+    # Statistical analysis
+    #
+    # Call any functions related to statistical analysis
+    ...
+
+    # Interactive pygame
+    #
+    # Create window and run interactive module
+    ...
