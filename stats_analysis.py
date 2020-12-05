acde35d3dc4bda46b0c89cfc134ce2e6fdb8a4c4
rk4life2018 <rk4life2018@gmail.com>
2020-12-05 11:15:10 -0500

    Plotly Graphing Compoind values

--- stats_analysis.py
+++ stats_analysis.py
@@ -1,7 +1,8 @@
 """All code for doing statiscal analysis on tweets."""
-from data_formatting import Tweet
+from data_formatting import Tweet, process
 from typing import List, Dict
 from vader_analysis import range_of_compound_values, vader_values
+import plotly.graph_objects as go
 
 
 def frequency(tweets: List[Tweet]) -> Dict[str, int]:
@@ -20,3 +21,15 @@ def frequency(tweets: List[Tweet]) -> Dict[str, int]:
         elif range_compound[0] <= tweet[1] <= range_compound[1]:
             freq_neu += 1
     return {'positive': freq_pos, 'neutral': freq_neu, 'negative': freq_neg}
+
+
+def normal_histogram(tweets: List[Tweet]) -> None:
+    """Displays a normal histogram in plotly containing the
+    compound values of each tweet in tweets.
+    """
+    compound_values = []
+    for tweet in tweets:
+        compound_values.append(vader_values(tweet, 'compound'))
+    fig = go.Figure(data=[go.Histogram(x=compound_values, histnorm='probability')])
+    fig.update_traces(xbins_size=0.01, selector=dict(type='histogram'))
+    fig.show()
