b5e8b5ee29e8d598f861533aa777a9f81d049fcf
Harsh Jaluka <jalukaharsh@gmail.com>
2020-12-05 14:58:21 +0000

    fixed range of tweets in frequency function

--- stats_analysis.py
+++ stats_analysis.py
@@ -7,7 +7,8 @@ from vader_analysis import range_of_compound_values, vader_values
 def frequency(tweets: List[Tweet]) -> Dict[str, int]:
     """Return the frequency of positive, negative and neutral tweets in the
     list of tweets provided."""
-    range_compound = range_of_compound_values(tweets)
+    news_neutral_tweets = [tweet for tweet in tweets if tweet.sentiment in {0,2}]
+    range_compound = range_of_compound_values(news_neutral_tweets)
     tweets_values = [(tweet, vader_values(tweet, 'compound')) for tweet in tweets]
     freq_neg = 0
     freq_pos = 0
