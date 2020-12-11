aa59b1aea731218256f4c94d616b417761bed8fc
Wilson Sy <sy.wilson@protonmail.com>
2020-12-11 10:39:57 -0500

    Rewrite frequency and add preconditions

--- stats_analysis.py
+++ stats_analysis.py
@@ -10,12 +10,17 @@ import statistics
 
 def frequency(tweets: List[Tweet]) -> Dict[str, int]:
     """Return the frequency of positive, negative and neutral tweets in the
-    list of tweets provided."""
+    list of tweets provided.
+    
+    Precondition:
+        - all(t.vader is not None for t in tweets)
+    """
     news_neutral_tweets = [tweet for tweet in tweets if tweet.sentiment in {0, 2}]
     range_compound = range_of_compound_values(news_neutral_tweets)
     freq_neg = 0
     freq_pos = 0
     freq_neu = 0
+    
     for tweet in tweets:
         if tweet.vader['compound'] < range_compound[0]:
             freq_neg += 1
@@ -23,12 +28,16 @@ def frequency(tweets: List[Tweet]) -> Dict[str, int]:
             freq_pos += 1
         elif range_compound[0] <= tweet.vader['compound'] <= range_compound[1]:
             freq_neu += 1
+    
     return {'positive': freq_pos, 'neutral': freq_neu, 'negative': freq_neg}
 
 
 def normal_histogram(tweets: List[Tweet]) -> None:
-    """Displays a normal histogram in plotly containing the
-    compound values of each tweet in tweets.
+    """Displays a normal histogram in plotly containing the compound values of each tweet
+     in tweets.
+
+    Precondition:
+        - all(t.vader is not None for t in tweets)
     """
     compound_values = []
     for tweet in tweets:
@@ -64,7 +73,7 @@ def plot_pos_neg(tweets: List[Tweet]) -> None:
     y-axis representing negative values.
     
     Preconditions:
-        - all(tweet.vader != None for tweet in tweets)
+        - all(tweet.vader is not None for tweet in tweets)
     """
     x_values = [tweet.vader['neg'] for tweet in tweets]
     y_values = [tweet.vader['pos'] for tweet in tweets]
