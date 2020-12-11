2d7167f3360334745b54f66035ab2cb72ed61a9f
Thomas <thomas.liu1303@gmail.com>
2020-12-11 10:56:57 -0500

    Added functions create graphs (used in pick_graph)

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Driver file for the Climate Change Sentiment on Twitter project.
 """
 
 import data_formatting
-import vader_analysis
 import stats_analysis
 import pick_graph
 
@@ -19,15 +18,29 @@ if __name__ == "__main__":
     pos_tweets = sorted_tweets[1]
     news_tweets = sorted_tweets[2]
 
-    # VADER sentiment analysis
-    vader_analysis.analyze(dataset)
-
-    # Statistical analysis
-    #
-    # Call any functions related to statistical analysis
-    ...
-
     # Interactive pygame
     #
     # Create window and run interactive module
     pick_graph.run_game()
+
+    # Statistical analysis
+    #
+    # Call any functions related to statistical analysis
+    def graph_neg() -> None:
+        """Graph negative tweets"""
+        stats_analysis.normal_histogram(neg_tweets)
+
+
+    def graph_pos() -> None:
+        """Graph positive tweets"""
+        stats_analysis.normal_histogram(neg_tweets)
+
+
+    def graph_neut() -> None:
+        """Graph neutral tweets"""
+        stats_analysis.normal_histogram(neg_tweets)
+
+
+    def graph_news() -> None:
+        """Graph news tweets"""
+        stats_analysis.normal_histogram(neg_tweets)
