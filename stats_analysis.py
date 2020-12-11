cc9dfba5702ccd4063fdfffac79c129d7bef4924
rk4life2018 <rk4life2018@gmail.com>
2020-12-11 11:38:53 -0500

    Added text to plotly histogram

--- stats_analysis.py
+++ stats_analysis.py
@@ -40,8 +40,36 @@ def normal_histogram(tweets: List[Tweet]) -> None:
         - all(t.vader is not None for t in tweets)
     """
     compound_values = [tweet.vader['compound'] for tweet in tweets]
+    summary_data = summary(compound_values)
+    lines = [
+        'Mean :' + str(summary_data['mean']),
+        'Median :' + str(summary_data['median']),
+        'Standard Deviation :' + str(summary_data['stdev']),
+        'Range :' + str(summary_data['range'])
+    ]
 
-    fig = go.Figure(data=[go.Histogram(x=compound_values, histnorm='probability')])
+    text = '<br>'.join(lines)  # Newlines represented by HTML '<br>' sequence
+
+    layout = go.Layout(
+        height=800,
+        width=800,
+        yaxis=go.layout.YAxis(domain=[0.5, 1]),
+        annotations=[
+            go.layout.Annotation(
+                bordercolor='black',  # Remove this to hide border
+                align='left',  # Align text to the left
+                yanchor='top',  # Align text box's top edge
+                text=text,  # Set text with '<br>' strings as newlines
+                showarrow=False,  # Hide arrow head
+                width=650,  # Wrap text at around 800 pixels
+                xref='paper',  # Place relative to figure, not axes
+                yref='paper',
+                font={'family': 'Courier'},  # Use monospace font to keep nice indentation
+                x=0,  # Place on left edge
+                y=0.4  # Place a little more than half way down
+            )
+        ])
+    fig = go.Figure(data=[go.Histogram(x=compound_values, histnorm='probability')], layout=layout)
     fig.update_traces(xbins_size=0.01, selector=dict(type='histogram'))
     fig.show()
 
