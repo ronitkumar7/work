16651e7eb351f3bdb486514abdb92eeecc9f8374
rk4life2018 <rk4life2018@gmail.com>
2020-12-12 11:45:43 -0500

    Updated Tweet attributes in stats_analysis

--- stats_analysis.py
+++ stats_analysis.py
@@ -21,18 +21,18 @@ def frequency(tweets: List[Tweet]) -> Dict[str, int]:
     Precondition:
         - all(t.vader is not None for t in tweets)
     """
-    news_neutral_tweets = [tweet for tweet in tweets if tweet.sentiment in {0, 2}]
-    range_compound = min_max_values([t.vader['compound'] for t in news_neutral_tweets])
+    news_neutral_tweets = [tweet for tweet in tweets if tweet.opinion in {0, 2}]
+    range_compound = min_max_values([t.sentiment['compound'] for t in news_neutral_tweets])
     freq_neg = 0
     freq_pos = 0
     freq_neu = 0
     
     for tweet in tweets:
-        if tweet.vader['compound'] < range_compound[0]:
+        if tweet.sentiment['compound'] < range_compound[0]:
             freq_neg += 1
-        elif tweet.vader['compound'] > range_compound[1]:
+        elif tweet.sentiment['compound'] > range_compound[1]:
             freq_pos += 1
-        elif range_compound[0] <= tweet.vader['compound'] <= range_compound[1]:
+        elif range_compound[0] <= tweet.sentiment['compound'] <= range_compound[1]:
             freq_neu += 1
     
     return {'positive': freq_pos, 'neutral': freq_neu, 'negative': freq_neg}
@@ -45,7 +45,7 @@ def normal_histogram(tweets: List[Tweet]) -> None:
     Precondition:
         - all(t.vader is not None for t in tweets)
     """
-    compound_values = [tweet.vader['compound'] for tweet in tweets]
+    compound_values = [tweet.sentiment['compound'] for tweet in tweets]
     summary_data = summary(compound_values)
     lines = [
         'Mean :' + str(summary_data['mean']),
@@ -107,8 +107,8 @@ def plot_pos_neg(tweets: List[Tweet]) -> None:
     Preconditions:
         - all(tweet.vader is not None for tweet in tweets)
     """
-    x_values = [tweet.vader['neg'] for tweet in tweets]
-    y_values = [tweet.vader['pos'] for tweet in tweets]
+    x_values = [tweet.sentiment['neg'] for tweet in tweets]
+    y_values = [tweet.sentiment['pos'] for tweet in tweets]
     fig = go.Figure(data=go.Scatter(x=x_values, y=y_values))
     fig.show()
     
@@ -118,10 +118,10 @@ def plot_compound(sorted_tweets: Dict[int, List[Tweet]]) -> None:
     and the y-coordinate is fixed for each tweet type 
     (-1 for 'does not support', 0 for 'neutral', 1 for 'supports' and 2 for 'news')
     """
-    not_support = [tweet.vader['compound'] for tweet in sorted_tweets[-1]]
-    neutral = [tweet.vader['compound'] for tweet in sorted_tweets[0]]
-    support = [tweet.vader['compound'] for tweet in sorted_tweets[1]]
-    news = [tweet.vader['compound'] for tweet in sorted_tweets[2]]
+    not_support = [tweet.sentiment['compound'] for tweet in sorted_tweets[-1]]
+    neutral = [tweet.sentiment['compound'] for tweet in sorted_tweets[0]]
+    support = [tweet.sentiment['compound'] for tweet in sorted_tweets[1]]
+    news = [tweet.sentiment['compound'] for tweet in sorted_tweets[2]]
     fig = go.Figure()
     # Use x instead of y argument for horizontal plot
     fig.add_trace(go.Box(x=not_support))
